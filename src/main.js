63c7eea2fe3ab9cd76e791d72251371e941c393d
myEm <1099582807@qq.com>
2019-07-09 16:36:04 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// main.js职责
+// 职责一：导入项目需要依赖的资源（css）
+// 职责二：初始化根实例（new vue创建实例，选项render作用：把某一个组件（app）渲染在指定容器$mount('#app')内）
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
