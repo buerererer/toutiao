829a0f2ee008c881429d922b429c4a1f5dbbbdd6
myEm <1099582807@qq.com>
2019-07-18 11:51:37 +0800

    富文本使用

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,11 +4,14 @@
       <div slot="header">
         <my-bread>发布文章</my-bread>
       </div>
-      <el-form :model="articleForm" label-width='100px'>
+      <el-form :model="articleForm" label-width="100px">
         <el-form-item label="标题：">
-          <el-input v-model='articleForm.title' style="width:400px"></el-input>
+          <el-input v-model="articleForm.title" style="width:400px"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="内容：">
+            富文本
+          <quill-editor v-model="articleForm.content"></quill-editor>
+        </el-form-item>
         <el-form-item label="封面：">
           <el-radio-group v-model="articleForm.cover.type">
             <el-radio :label="1">单图</el-radio>
@@ -16,22 +19,27 @@
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
-          <div class='img-btn'><img src="../../assets/images/default.png" alt=""></div>
+          <div class="img-btn">
+            <img src="../../assets/images/default.png" alt />
+          </div>
         </el-form-item>
         <el-form-item label="频道：">
-            <my-channel v-model='articleForm.channel_id'></my-channel>
+          <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary">发表</el-button>
-            <el-button >存入草稿</el-button>
+          <el-button type="primary">发表</el-button>
+          <el-button>存入草稿</el-button>
         </el-form-item>
-
       </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 export default {
   data () {
     return {
@@ -40,21 +48,24 @@ export default {
         title: [],
         content: '',
         cover: {
-          type: 1,
-          images: []
+          type: 1
+        //   images: []
         },
         channel_id: null
       }
     }
+  },
+  components: {
+    quillEditor
   }
 }
 </script>
 
 <style scope lang='less'>
 .img-btn {
-    width: 150px;
-    height: 150px;
-    border: 1px soli #ddd;
-    display: block;
+  width: 150px;
+  height: 150px;
+  border: 1px soli #ddd;
+  display: block;
 }
 </style>
